fe017eb1cb06c79a711aebae72307f0e4d121175
Lucas Almeida <lucasaclima03@gmail.com>
2021-10-12 19:33:58 -0300

    pagina de login e funções de validação do login criados

--- models/Auth.php
+++ models/Auth.php
@@ -29,4 +29,23 @@ class Auth {
 
     }
 
+    public function validateLogin($email, $password){
+        $userDao = new UserDaoMysql($this->pdo);
+        $user = $userDao->findByEmail($email);
+        if($user) {
+            if(password_verify($password, $user->password)){
+                $token = md5(time().rand(0,9999));
+
+                $_SESSION['token'] = $token;
+                $user->token = $token;
+                $userDao->update($user);
+
+                return true;
+            }
+        }
+
+        return false;
+
+    }
+
 } 
