d114690bd7d16628eb7b6375d58f1c503a82170b
Lucas Almeida <lucasaclima03@gmail.com>
2021-10-12 15:09:06 -0300

    criação do DAO do usuário

--- models/Auth.php
+++ models/Auth.php
@@ -1,18 +1,26 @@
 <?php
+require_once 'dao/UserDaoMysql.php';
 class Auth {
 
     private $pdo;
     private $base;
 
     public function __construct($pdo, $base) {
-        $this->$pdo = $pdo;
-        $this->$base = $base;
+        $this->pdo = $pdo;
+        $this->base = $base;
 
     }
 
     public function checkToken() {
         if(!empty($_SESSION['token'])){
             $token = $_SESSION['token'];
+            
+            $userDao = new UserDaoMysql($this->pdo);
+            $user = $userDao->findByToken($token);
+
+            if($user) {
+                return $user;
+            }
 
         }
 
